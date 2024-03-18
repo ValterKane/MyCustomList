0836e95445fcbfa4f346168a6518512fc02e8213
ValterKane <zhukov.petr86@yandex.ru>
2024-03-18 21:59:16 +0300

    Ready to work

--- MyCustomList.cs
+++ MyCustomList.cs
@@ -2,6 +2,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -9,26 +10,59 @@ namespace MyPersonArray
 {
     public class MyCustomList<T> : IEnumerable<T>, ICollection<T>, ICollection
     {
+        // Внутренее хранилище данных
         internal T[]? _innerArray;
+        // Размер внутреннего хранилища
         internal int _size;
+        // Текущая версия объекта
         internal int _version;
 
+        // Пустая область памяти
         private static readonly T[] EmptyArray = new T[0];
 
+        // Своства количества элементов в списке
         public int Count => _size;
 
-        public bool IsReadOnly => false;
+        // Свойство вместимости коллекци
+        public int Capacity
+        {
+            get => _innerArray.Length;
+            set
+            {
+                if (value < 0) throw new ArgumentOutOfRangeException("value");
+                else
+                {
+                    if (value != _innerArray.Length)
+                    {
+                        if (value > 0)
+                        {
+                            T[] newItems = new T[value];
+                            if (_size > 0)
+                            {
+                                Array.Copy(_innerArray, newItems, _size);
+                            }
+                            _innerArray = newItems;
+                        }
+                        else
+                        {
+                            _innerArray = EmptyArray;
+                        }
+                    }
+                }
+            }
+        }
 
-        public bool IsSynchronized => false;
+        // Флаги списка
+        public bool IsReadOnly => false; // Списк только для чтения?
+        public bool IsSynchronized => false; // Список синхронизирован?
+        public object SyncRoot => this; // Где находится корень синхронизации?
 
-        public object SyncRoot => this;
-
+        // Конструкторы
         public MyCustomList()
         {
             _innerArray = EmptyArray;
             _size = 0;
         }
-
         public MyCustomList(int capacity)
         {
             if (capacity < 0)
@@ -43,7 +77,6 @@ namespace MyPersonArray
                _size = capacity;
             }     
         }
-
         public MyCustomList(IEnumerable<T> collection)
         {
             if (collection == null)
@@ -77,8 +110,8 @@ namespace MyPersonArray
             }
         }
 
-
-
+        // Метод добавления элементов в конец
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public void Add(T item)
         {
             _version++;
@@ -95,6 +128,7 @@ namespace MyPersonArray
             }
         }
 
+        #region Методы, которые нужно реализовать (Lvl-Noob)
         public void AddRange(IEnumerable<T> collection)
         {
             throw new NotImplementedException();
@@ -120,20 +154,7 @@ namespace MyPersonArray
             throw new NotImplementedException();
         }
 
-        public IEnumerator<T> GetEnumerator()
-        {
-            for (int i = 0; i < _size; i++)
-            {
-                yield return _innerArray[i];
-            }
-        }
-
         public bool Remove(T item)
-        {
-            throw new NotImplementedException();
-        }
-
-        IEnumerator IEnumerable.GetEnumerator()
         {
             throw new NotImplementedException();
         }
@@ -142,5 +163,58 @@ namespace MyPersonArray
         {
             throw new NotImplementedException();
         }
+
+        public void Insert(int index, T item) => throw new NotImplementedException();
+        #endregion
+
+        #region Методы, которые нужно реализовать (Lvl-PRO)
+        // Метод, который конвертирует все объекты списка из одного типа в другой
+        public List<TOutput> ConvertAll<TOutput>(Converter<T, TOutput> converter) => throw new NotImplementedException();
+        public int EnsureCapacity(int capacity) => throw new NotImplementedException();
+        public bool Exists(Predicate<T> match) => throw new NotImplementedException();
+        public T? Find(Predicate<T> match) => throw new NotImplementedException();
+        public List<T> FindAll(Predicate<T> match) => throw new NotImplementedException();
+        public int BinarySearch(int index, int count, T item, IComparer<T>? comparer) => throw new NotImplementedException();
+        public void QuickSort(bool isASC) => throw new NotImplementedException();
+
+        #endregion
+
+
+        // Итераторы
+        public IEnumerator<T> GetEnumerator()
+        {
+            for (int i = 0; i < _size; i++)
+            {
+                yield return _innerArray[i];
+            }
+        }
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return _innerArray.GetEnumerator();
+        }
+
+        // Индексатор
+        public T this[int index]
+        {
+            get
+            {
+                if((uint) index > _size)
+                    throw new ArgumentOutOfRangeException();
+
+                return _innerArray[index];
+            }
+
+            set 
+            {
+                if((uint)index > _size)
+                    throw new ArgumentOutOfRangeException();
+
+                _innerArray[index] = value;
+                _version++;
+
+            }
+        }
+
+        
     }
 }
